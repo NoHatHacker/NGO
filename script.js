e90941395a7775ff71d1375774caff6b45e8cc7c
Partha Pratim Biswas <154015744+NoHatHacker@users.noreply.github.com>
2024-01-13 19:51:49 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
 gsap.from(".content-left",{
-    x:-550,
-    duration:2,
+    x:-100,
+    duration:1.8,
     delay: 0.5,
     opacity: 0
 })
 
 gsap.from(".content-right",{
-    x:550,
-    duration: 2,
+    x:100,
+    duration: 1.8,
     delay:  0.5,
     opacity: 0
 })
 gsap.from(".nav",{
-    y:-100,
+    y:-50,
     duration:2,
     delay: 0.3,
     opacity: 0,
